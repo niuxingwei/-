84024a56447819bf59b6b627b440408a690ad09c
e <1669250716@qq.com>
2019-12-02 11:27:02 +0800

    登陆界面修改

--- src/views/Login/login.vue
+++ src/views/Login/login.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author:牛兴炜
  * @Date: 2019-10-23 21:43:00
- * @LastEditTime: 2019-11-28 14:47:23
+ * @LastEditTime: 2019-11-28 23:33:23
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \simple-login-master\src\views\Login\index.vue
@@ -96,7 +96,7 @@ export default {
             if (result.length === 0) {
               this.$message.error("该用户未注册，请先注册！")
             }
-            if (result[0].account === this.loginForm.userAccount && result[0].pwd === this.loginForm.userPassword) {
+            if (result[0].pass === this.loginForm.userPassword) {
               this.$router.push('home')
             } else {
               this.$message.error("用户名或密码不正确，请检查！")
