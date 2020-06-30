9cc36b1bf7dd329376979313be969b8d9da020b7
e <1669250716@qq.com>
2020-06-30 11:19:08 +0800

    代码调整

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,10 +1,18 @@
+/*
+ * @Author: your name
+ * @Date: 2020-06-30 10:51:19
+ * @LastEditTime: 2020-06-30 11:17:20
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \webFont\.postcssrc.js
+ */ 
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
   "plugins": {
     "postcss-import": {},
     "postcss-url": {},
-    // to edit target browsers: use "browserslist" field in package.json
+     // to edit target browsers: use "browserslist" field in package.json
     "autoprefixer": {}
   }
 }
